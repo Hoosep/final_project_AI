3fc560d642395b5192a96451fc57775915346df0
Hoose <hoose.muu@gmail.com>
2021-08-11 23:12:57 -0500

    ADD: Variables.

---
+++ dataset/variables.py
@@ -0,0 +1,12 @@
+STATES = ['Jalisco', 'Chihuahua', 'Sinaloa', 'Ciudad de Mexico', 'Michoacan', 'Nuevo Leon']
+YEARS = ['2014', '2015', '2016', '2017', '2018', '2019']
+
+
+DATA = [
+    [403, 284, 301, 316, 351, 291], # Jalisco
+    [235, 317, 286, 268, 270, 263], # Chihuahua
+    [234, 285, 255, 275, 278, 236], # Sinaloa
+    [291, 200, 220, 193, 223, 220], # Ciudad de Mexico
+    [298, 252, 286, 189, 171, 212], # Michoacan
+    [226, 202, 245, 223, 229, 204]  # Nuevo Leon
+]
