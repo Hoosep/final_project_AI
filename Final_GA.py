86b8cd56558fb58607663e4e90fd935b49a1a97e
PGONZA95 <pgonza95@ford.com>
2021-08-19 15:51:00 -0500

    Funciones adaptadas: CreatePopulation, Elitism, Breed, Mutate Falta: Calc error, Graficar

--- Final_GA.py
+++ Final_GA.py
@@ -1,10 +1,153 @@
+from typing import Coroutine
 import numpy as np, random, operator, pandas as pd, sys, matplotlib.pyplot as plt, matplotlib.cm as cm, math, time
 from mpl_toolkits import mplot3d
 
 from mpl_toolkits.mplot3d import Axes3D
+
+POPULATION_SIZE = 1
+CROMOSOME_SIZE = 39
+tournamentSize = 5
 X = []
 Y = []
 Z = []
+parents = []
+
+def CreatePopulation(parents):    
+    for i in range(0, POPULATION_SIZE):
+        cromosome = []
+        for genIndex in range(0,CROMOSOME_SIZE):
+            randomGen = random.randint(5,255)
+            cromosome.append(randomGen)
+        # print(len(cromosome))
+        parents.append(cromosome)
+        #Print adjusted values
+        for gen in cromosome:
+            print(gen/5)
+
+def Mask_Number(value, n):
+    return value & ((1 << n) - 1)
+
+def SwapBytes(parent1, parent2, startByte):
+    #print(startByte)
+    while startByte < len(parent1):
+        
+        temp = parent2[startByte]
+        parent2[startByte] = parent1[startByte]
+        parent1[startByte] = temp
+        startByte += 1
+    return parent1, parent2
+
+def Breed(parent1, parent2):
+    # del(parent2[-1])
+    # del(parent1[-1])
+    #print(len(parent1))
+    cut = random.randint(0, int(CROMOSOME_SIZE*8)-2)
+    #print(cut)
+    #cut = 24
+    startByte = int(cut/8)
+
+    # print("Parent1: ", parent1)
+    # print("Parent2: ", parent2)
+
+    child1 = []
+    child2 = []
+    y = []
+    yy = []
+    if cut % 8 != 0:
+        
+        modulus = cut % 8
+        bitsToCut = 8 - modulus        
+        # print("Bit to cut: ", cut)
+        # print("Bit position: ", modulus)
+        # print("Bits to cut: ", bitsToCut)
+        # print("Cut is not zero: ",cut," Start byte:", startByte )
+        yByte = parent1[startByte]
+        yyByte = parent2[startByte]
+        # print("YByte: ", bin(yByte))
+        # print("YYByte: ", bin(yyByte))
+        yCut = Mask_Number(yByte, bitsToCut)
+        yyCut = Mask_Number(yyByte, bitsToCut)
+        yFinal = ((yByte >> bitsToCut) << bitsToCut) | yyCut
+        yyFinal = ((yyByte >> bitsToCut) << bitsToCut) | yCut
+        # print("YCut: ", bin(yCut))
+        # print("YYCut: ", bin(yyCut))
+        # print("YFinal: ", bin(yFinal))
+        # print("YYFinal: ", bin(yyFinal))
+        parent1[startByte] = yFinal
+        parent2[startByte] = yyFinal
+        child1, child2 = SwapBytes(parent1, parent2, startByte)
+        # print(child1)
+        # print(child1)
+        
+    else:
+        child1, child2 = SwapBytes(parent1, parent2, startByte)
+        # print(parent1)
+        # print(parent2)
+        
+    # print("Child1: ", child1)
+    # print("Child2: ", child2)    
+    return child1, child2
+
+def Tournament(parents):
+    winner1 = []
+    winner2 = []
+    distanceToBeat = sys.maxsize
+
+    tournamentList1 =  random.sample(parents, tournamentSize)
+    tournamentList2 =  random.sample(parents, tournamentSize)
+    for element in tournamentList1:
+        #print(element)
+        if element[-1] < distanceToBeat:
+            winner1 = element
+    for element in tournamentList2:
+        #print(element)
+        if element[-1] < distanceToBeat:
+            winner2 = element
+    # print("W1: ", winner1)
+    # print("W2: ", winner2)
+    return winner1, winner2
+
+def CalculateError(population):
+    for cromosome in population:
+        m1 = cromosome[0]/5
+        m2 = cromosome[1]/5
+        m3 = cromosome[2]/5
+        m4 = cromosome[3]/5
+        m5 = cromosome[4]/5
+        m6 = cromosome[5]/5
+        de1 = cromosome[6]/5
+        de2 = cromosome[7]/5
+        de3 = cromosome[8]/5
+        de4 = cromosome[9]/5
+        de5 = cromosome[10]/5
+        de6 = cromosome[11]/5
+        p1 = cromosome[12]/5
+        p2 = cromosome[13]/5
+        p3 = cromosome[14]/5
+        p4 = cromosome[15]/5
+        p5 = cromosome[16]/5
+        p6 = cromosome[17]/5
+        p7 = cromosome[18]/5
+        p8 = cromosome[19]/5
+        p9 = cromosome[20]/5
+        q1 = cromosome[21]/5
+        q2 = cromosome[22]/5
+        q3 = cromosome[23]/5
+        q4 = cromosome[24]/5
+        q5 = cromosome[25]/5
+        q6 = cromosome[26]/5
+        q7 = cromosome[27]/5
+        q8 = cromosome[28]/5
+        q9 = cromosome[29]/5
+        r1 = cromosome[30]/5
+        r2 = cromosome[31]/5
+        r3 = cromosome[32]/5
+        r4 = cromosome[33]/5
+        r5 = cromosome[34]/5
+        r6 = cromosome[35]/5
+        r7 = cromosome[36]/5
+        r8 = cromosome[37]/5
+        r9 = cromosome[38]/5
 
 def CreateGraph():
     m1=0
@@ -86,12 +229,14 @@ def CreateGraph():
     print(X)
     fig = plt.figure()
     ax = plt.axes(projection='3d')
-
+    # Z = np.array(DATA)
+    # ax.plot_surface(XGraph, YGraph, Z,cmap='viridis', edgecolor='none')
     ax.plot_trisurf(X, Y, Z, linewidth=0, antialiased=False)
     ax.set_title('Surface plot')
     plt.show()
 
 def main():
-    CreateGraph()
+    # CreateGraph()
+    CreatePopulation(parents)
 if __name__=="__main__":
     main()
